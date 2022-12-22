391431df79b4ed1a68ed6fa4a14f3b73bda9740a
Yusupov Jaloliddin <92913529+YusupovJaloliddin@users.noreply.github.com>
2022-12-22 23:55:56 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
+  //upadted auth
   await app.listen(3000);
 }
 bootstrap();
